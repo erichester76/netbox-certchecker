3827e8fe4e6ddd4fae9a1a4280d21108fa3657f0
erichester76 <eric.hester@gmail.com>
2024-10-05 12:18:35 -0400

    Update setup.py to add pyOpenSSL requirement

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='netbox-certchecker',
     version='1.0',
     description='Netbox plugin for checking the certificate expiration date',
-    install_requires=[],
+    install_requires=[pyOpenSSL],
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
