9c282a7e97a267857cc160e65903a888ce47b4bf
Eric Hester <eric.hester@umbrella.tech>
2024-10-16 19:01:52 -0400

    added menu items for new objects

--- netbox_certificate/navigation.py
+++ netbox_certificate/navigation.py
@@ -1,19 +1,36 @@
 from netbox.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
 
 
-certificate_buttons = [
-    PluginMenuButton(
-        link='plugins:netbox_certificate:certificate_add',
-        title='Add',
-        icon_class='mdi mdi-plus-thick',
-    )
-]
-
 cert_items = (
+    PluginMenuItem(
+        link='plugins:netbox_certificate:ca_list',
+        link_text='Certificate Authorities',
+        buttons=[
+            PluginMenuButton(
+            link='plugins:netbox_certificate:ca_add',
+            title='Add',
+            icon_class='mdi mdi-plus-thick',
+        )]
+    ),
     PluginMenuItem(
         link='plugins:netbox_certificate:certificate_list',
         link_text='Certificates',
-        buttons=certificate_buttons
+        buttons=[
+            PluginMenuButton(
+            link='plugins:netbox_certificate:certificate_add',
+            title='Add',
+            icon_class='mdi mdi-plus-thick',
+        )]
+    ),
+    PluginMenuItem(
+        link='plugins:netbox_certificate:hostname_list',
+        link_text='Hostnames',
+        buttons=[
+            PluginMenuButton(
+            link='plugins:netbox_certificate:hostname_add',
+            title='Add',
+            icon_class='mdi mdi-plus-thick',
+        )]
     ),
 )
 
