779872cc67be1f3e50f3851ce19b763747e08abd
Eric Hester <eric.hester@umbrella.tech>
2024-10-16 16:44:42 -0400

    fixed navigation

--- netbox_certchecker/navigation.py
+++ netbox_certchecker/navigation.py
@@ -1,4 +1,4 @@
-from netbox.plugins import PluginMenuButton, PluginMenuItem
+from netbox.plugins import PluginMenuButton, PluginMenuItem, PluginMenu
 from utilities.choices import ButtonColorChoices
 
 certchecker_buttons = [
@@ -6,14 +6,21 @@ certchecker_buttons = [
         link='plugins:netbox_certchecker:certificate_add',
         title='Add',
         icon_class='mdi mdi-plus-thick',
-        color=ButtonColorChoices.GREEN
     )
 ]
 
-menu_items = (
+
+
+cert_items = (
     PluginMenuItem(
         link='plugins:netbox_certchecker:certificate_list',
-        link_text='Certchecker',
+        link_text='Certificates',
         buttons=certchecker_buttons
     ),
+)
+
+menu = PluginMenu(
+    label="Certificates",
+    groups=(("CERTIFICATES", cert_items),),
+    icon_class="mdi mdi-lan",
 )
